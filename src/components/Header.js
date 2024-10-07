047d27e3ec3f8e480a9fbe741c934d91c0171007
Mahadali-miryam <mahadali-miryam@outlook.com>
2024-10-07 10:51:07 +0200

    fin header

---
+++ src/components/Header.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import '../style/header.scss';
+
+const Header =() => {
+  return (
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+        <NavLink className="navbar-brand" to="/">   
+            <img src="Logo.png" alt="Logo"/>
+        </NavLink>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+            <NavLink className="nav-link" aria-current="page" to="/Home" activeClassName="active">Accueil</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/Services" activeClassName="active">Batiment</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/Projects" activeClassName="active">Services</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/Blog" activeClassName="active">Fabrication</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/Contact" activeClassName="active">Alimentation</NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Header;
